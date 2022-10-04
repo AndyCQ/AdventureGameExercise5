8dd8b9842ab769f1601a57792a2dcc571453a875
William Yang <wy2037@nyu.edu>
2022-10-04 01:35:05 -0400

    Door and Key System
    Finished a proper door and key system (self implementation)

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public Door count;
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+           Destroy(gameObject);
+           count.keyCount += 1;
+        }
+    }
+}
