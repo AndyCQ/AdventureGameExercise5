7d0dc304ccfed3926514d20f55f4603fa602146e
William Yang <wy2037@nyu.edu>
2022-10-06 01:47:25 -0400

    Bomb Part 1

--- Assets/Scripts/PlayerCode.cs
+++ Assets/Scripts/PlayerCode.cs
@@ -4,8 +4,11 @@ using UnityEngine.AI;
 public class PlayerCode : MonoBehaviour
 {
     public GameObject sword;
+    public GameObject bomb;
     public NavMeshAgent agent;
     public float swordSpeed = 100;
+    public float bombSpeed = 100;
+    public float bombs = 3;
     Camera mainCam;
 
     void Start()
@@ -22,11 +25,22 @@ public class PlayerCode : MonoBehaviour
             }
         }
         if(Input.GetMouseButtonDown(0)){
+            /*
             RaycastHit hit;
             if(Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 200)) {
                 transform.LookAt(hit.point);
-                GameObject newSword = Instantiate(sword,transform.position,transform.rotation);
+                GameObject newSword = Instantiate(sword,transform.position);
                 newSword.GetComponent<Rigidbody>().AddForce(transform.forward * swordSpeed);
+            }
+            */
+            //sword slash since regular sword system doesn't work
+        }
+        if(Input.GetKeyDown("space") & bombs > 0) {
+            RaycastHit hit;
+            if(Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 200)) {
+                transform.LookAt(hit.point);
+                GameObject newBomb = Instantiate(bomb,transform.position,transform.rotation);
+                newBomb.GetComponent<Rigidbody>().AddForce(transform.forward * bombSpeed);
             }
         }
     }
