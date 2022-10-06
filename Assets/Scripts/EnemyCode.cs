8c1baecefcf6d7a56e1d0d4f1eec9107a200cbb9
William Yang <wy2037@nyu.edu>
2022-10-06 19:44:29 -0400

    Some stuff
    Change enemies to slime prefab
    Modified health script to work with multiple enemies

--- Assets/Scripts/EnemyCode.cs
+++ Assets/Scripts/EnemyCode.cs
@@ -5,16 +5,32 @@ using UnityEngine.AI;
 public class EnemyCode : MonoBehaviour
 {
     public GameObject player;
+    public PlayerCode playerHP;
     public NavMeshAgent agent;
+    public float damageAmt = 1f;
+    public float hitCooldown = 1f;
 
     void Start() {
         StartCoroutine(LookForPlayer());
+    }
+
+    void FixedUpdate() {
+        if (hitCooldown > 0) {
+            hitCooldown -= Time.deltaTime;
+        }
     }
 
     IEnumerator LookForPlayer() {
         while(true) {
             yield return new WaitForSeconds(0.5f);
             agent.SetDestination(player.transform.position);
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player" && hitCooldown <= 0) {
+            hitCooldown = 1f;
+            playerHP.playerDamage(damageAmt);
         }
     }
 }
