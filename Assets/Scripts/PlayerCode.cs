bbde7922fe123201cb2709f0338ce07428051fd8
William Yang <wy2037@nyu.edu>
2022-10-07 03:26:44 -0400

    Fixes

--- Assets/Scripts/PlayerCode.cs
+++ Assets/Scripts/PlayerCode.cs
@@ -54,7 +54,6 @@ public class PlayerCode : MonoBehaviour
         }
         if(Input.GetKeyDown("space") & bombs > 0) {
             bombs -= 1;
-            bombAmount.text = "x " + bombs;
             RaycastHit hit;
             if(Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 200)) {
                 transform.LookAt(hit.point);
@@ -62,6 +61,7 @@ public class PlayerCode : MonoBehaviour
                 newBomb.GetComponent<Rigidbody>().AddForce(transform.forward * bombSpeed);
             }
         }
+        bombAmount.text = "x " + bombs;
     }
 
     void refreshTimer() {
