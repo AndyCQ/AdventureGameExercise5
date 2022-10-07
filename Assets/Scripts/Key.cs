8d79ca93a443f216abf459cb690f5668b0c4e04d
NiravGolyalla <ng2411@nyu.edu>
2022-10-07 03:39:21 -0400

    Stash

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public Door count;
     void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
            Destroy(gameObject);
            count.keyCount += 1;
+=======
+    public Door count = null;
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+           Destroy(gameObject);
+           if(count != null){
+            count.keyCount += 1;
+           }
+>>>>>>> Stashed changes
         }
     }
 }
