636f414144c8766874692767a3b6748d7b31de40
NiravGolyalla <ng2411@nyu.edu>
2022-10-07 08:02:12 -0400

    Safety Backup in Case I mess up
    some warning scare me

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,6 +4,7 @@ public class EnemyHealth : MonoBehaviour
 {
     public float maxHealth = 3f;
     public float currentHealth;
+    public GameObject player;
 
     void Start()
     {
@@ -18,6 +19,15 @@ public class EnemyHealth : MonoBehaviour
     {
         if (currentHealth <= 0f) {
             Debug.Log("Dies from cringe");
+            if(gameObject.tag == "Special"){
+                gameObject.GetComponent<MobDrop>().specialDrop();
+            }
+            if(gameObject.tag == "Boss"){
+                gameObject.GetComponent<MobDrop>().bossDrop();
+            }
+            print(player.GetComponent<PlayerCode>().health);
+            player.GetComponent<PlayerCode>().health += 1;
+            print(player.GetComponent<PlayerCode>().health);
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/MobDrop.cs
+++ Assets/Scripts/MobDrop.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mobDrop : MonoBehaviour
+public class MobDrop : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Door link;
+    public GameObject key;
+    public GameObject crown;
+    
+    public void specialDrop() {
+        GameObject newKey = Instantiate(key,gameObject.transform.position,gameObject.transform.rotation);
+        newKey.GetComponent<Key>().count = link;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void startBattle() {
+        //link.GetComponent<MeshRenderer>().enabled = true;
+        //link.GetComponent<NavMeshObstacle>().enabled = true;
+        //gameObject.GetComponent<Navmesh
+    }
+
+    public void bossDrop() {
+        GameObject newCrown = Instantiate(crown,gameObject.transform.position,gameObject.transform.rotation);
+        startBattle();
     }
 }
